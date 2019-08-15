63a1867dc0349253d02c0d533c2eb344237a4be6
Keenan <duncanyoyo1@gmail.com>
2019-08-14 20:43:01 -0400

    Change to QMK RGB & remove legacy RGB from keymap

--- keyboards/massdrop/ctrl/keymaps/duncanyoyo1/config_led.c
+++ keyboards/massdrop/ctrl/keymaps/duncanyoyo1/config_led.c
@@ -1,9 +1,6 @@
 /*
 Copyright 2019 Keenan C. <duncanyoyo1@gmail.com>
 
-Thanks to MatthewRobo for the implementation details
-& Massdrop for the config_led.c
-
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
--- keyboards/massdrop/ctrl/keymaps/duncanyoyo1/rules.mk
+++ keyboards/massdrop/ctrl/keymaps/duncanyoyo1/rules.mk
@@ -1,3 +1,2 @@
 # This keymap may require Massdrop Configurator support
-OPT_DEFS += -DUSE_MASSDROP_CONFIGURATOR
 SRC += config_led.c
